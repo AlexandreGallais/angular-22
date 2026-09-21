d19b4c7266a0a367aebda958a375715837b0bedb
Alexandre GALLAIS <37043327+AlexandreGallais@users.noreply.github.com>
2026-09-21 16:54:37 +0200

    s

--- lib/.storybook/main.ts
+++ lib/.storybook/main.ts
@@ -1,10 +1,8 @@
 import type { StorybookConfig } from '@storybook/angular-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [],
-  "framework": "@storybook/angular-vite"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-themes', '@storybook/addon-actions/register'],
+  framework: '@storybook/angular-vite',
 };
 export default config;
