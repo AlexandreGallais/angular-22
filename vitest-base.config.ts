decbf0688f94c95b64c61136fca86405f31f3a3e
Alexandre GALLAIS <37043327+AlexandreGallais@users.noreply.github.com>
2026-09-22 19:16:46 +0200

    t

--- vitest-base.config.ts
+++ vitest-base.config.ts
@@ -1,9 +1,22 @@
-// Learn more about Vitest configuration options at https://vitest.dev/config/
-
+// vitest-base.config.ts
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    // ...
+    passWithNoTests: true,
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.spec.ts', 'src/**/*.d.ts', 'src/main.ts', 'src/environments/**'],
+      reporter: ['json'],
+      reportsDirectory: './coverage/app',
+      thresholds: {
+        statements: 0,
+        branches: 0,
+        functions: 0,
+        lines: 0,
+      },
+    },
   },
 });
